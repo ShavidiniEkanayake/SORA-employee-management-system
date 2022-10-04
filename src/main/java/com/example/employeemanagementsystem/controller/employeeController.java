d0a5b4d6fd586b351cee6c7ef767cf1567213219
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-10-05 01:02:51 +0530

    Completed get employee part

--- src/main/java/com/example/employeemanagementsystem/controller/employeeController.java
+++ src/main/java/com/example/employeemanagementsystem/controller/employeeController.java
@@ -5,6 +5,8 @@ import com.example.employeemanagementsystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/employee")
 @CrossOrigin
@@ -12,13 +14,24 @@ public class employeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/getEmployee")
-    public String getEmployee(){
-        return "Root";
+    @GetMapping("/getAllEmployee")
+    public List<EmployeeDTO> getAllEmployee(){
+        return employeeService.getAllEmployee();
     }
 
     @PostMapping("/saveEmployee")
     public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO){
         return employeeService.saveEmployee(employeeDTO);
     }
+
+    @PutMapping("/updateEmployee/{id}")
+    public void updateEmployee(@RequestBody EmployeeDTO employeeDTO, @PathVariable int id){
+        employeeService.updateEmployee(id,employeeDTO);
+    }
+
+    @DeleteMapping("/deleteEmployee/{id}")
+    public void deleteEmployee(@PathVariable("id") int id){
+        this.employeeService.deleteEmployee(id);
+    }
+
 }
--- src/main/java/com/example/employeemanagementsystem/service/EmployeeService.java
+++ src/main/java/com/example/employeemanagementsystem/service/EmployeeService.java
@@ -3,23 +3,43 @@ package com.example.employeemanagementsystem.service;
 import com.example.employeemanagementsystem.dto.EmployeeDTO;
 import com.example.employeemanagementsystem.entity.Employee;
 import com.example.employeemanagementsystem.repo.EmployeeRepo;
+import net.bytebuddy.description.method.MethodDescription;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
-
+    private  List<Employee>employeeList= new ArrayList<>();
     @Autowired
     private ModelMapper modelMapper;
 
     public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO){
         employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
         return employeeDTO;
+    }
+
+    public List<EmployeeDTO> getAllEmployee(){
+        List<Employee>employeeList= employeeRepo.findAll();
+        return modelMapper.map(employeeList, new TypeToken<List<EmployeeDTO>>(){}.getType());
+    }
+
+    public void updateEmployee(int id,EmployeeDTO employeeDTO){
+
+    }
+
+    public void deleteEmployee(int id){
+//        employeeRepo.delete(modelMapper.map(id,Employee.class));
+        employeeList = employeeList.stream().filter(employeeDTO-> employeeDTO.getId()!=id).
+                collect(Collectors.toList());
     }
 }
