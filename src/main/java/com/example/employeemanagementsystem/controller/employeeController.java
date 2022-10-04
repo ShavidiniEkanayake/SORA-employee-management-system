d8d538bcf15079b079891f6903ecdb6688314f73
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-10-04 15:04:49 +0530

    Added controller package

---
+++ src/main/java/com/example/employeemanagementsystem/controller/employeeController.java
@@ -0,0 +1,17 @@
+package com.example.employeemanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/employee")
+@CrossOrigin
+public class employeeController {
+
+    @GetMapping("/getEmployee")
+    public String getEmployee(){
+        return "Root";
+    }
+}
